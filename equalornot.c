e1ae6a007fde1389db60ddc463c351a5fe3757f0
Dhilleswara Rao <88971652+tdr1506@users.noreply.github.com>
2023-02-20 12:26:28 +0530

    Create equalornot.c

---
+++ equalornot.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+void main()
+{
+    int num1, num2;
+ 
+    printf("Input the values for Number1 and Number2 : ");
+    scanf("%d %d", &num1, &nunm2);
+    if (num1 == num2)
+        printf("Number1 and Number2 are equal\n");
+    else
+        printf("Number1 and Number2 are not equal\n");
+}
