dc625cba206f4be5072cd6e9bd8e86a9117610b0
Dhilleswara Rao <88971652+tdr1506@users.noreply.github.com>
2023-02-21 09:59:23 +0530

    Update equalornot.c

--- equalornot.c
+++ equalornot.c
@@ -3,10 +3,10 @@ void main()
 {
     int num1, num2;
  
-    printf("Input the values for Number1 and Number2 : ");
+    printf("Enter any two numbers: ");
     scanf("%d %d", &num1, &nunm2);
     if (num1 == num2)
-        printf("num1 and num2 are equal\n");
+        printf("Both are equal\n");
     else
-        printf("num1 and num2 are not equal\n");
+        printf("Both are not equal\n");
 }
